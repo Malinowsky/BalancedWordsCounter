db7c19781e2ccac57d33970e7732bb5e933878c7
Artur <arturrmalinowski@gmail.com>
2022-06-05 21:20:08 +0200

    complete

--- src/main/java/functions/BalancedWordsCounter.java
+++ src/main/java/functions/BalancedWordsCounter.java
@@ -1,2 +1,59 @@
-package functions;public class BalancedWordsCounter {
+package functions;
+
+import java.util.*;
+
+public class BalancedWordsCounter {
+    public int count(String input){
+
+        int sum=0;
+
+        //if input is null throw exception
+        if(input != null){
+            for(int i=1; i<=input.length(); i++){
+
+                for(int j=0; j<=input.length(); j++){
+
+                    if(input.length() >= j + i){
+                        sum += check(input.substring(j, j+i));
+                    }else{
+                        break;
+                    }
+                }
+            }
+        }else{
+            throw new RuntimeException("RuntimeException");
+        }
+        return sum;
+    }
+    private int check(String subword) {
+
+        int scount=-1;
+
+        for (int i = 0; i < subword.length(); i++){
+
+            char charAt = subword.charAt(i);
+            int count=0;
+
+            for (int j = 0; j < subword.length(); j++){
+
+                if(Character.isLetter(subword.charAt(j))==false)
+                {
+                    throw new RuntimeException("RuntimeException");
+                }
+
+                if (subword.charAt(j) == charAt){
+                    count++;
+                }
+            }
+
+            if(scount==-1){
+                scount = count;
+            }else if (scount != count){
+                return 0;
+            }
+
+        }
+
+        return 1;
+    }
 }
--- src/test/java/functions/BalancedWordsCounterTest.java
+++ src/test/java/functions/BalancedWordsCounterTest.java
@@ -1,4 +1,22 @@
-import static org.junit.jupiter.api.Assertions.*;
-class BalancedWordsCounterTest {
-  
+package functions;
+
+public class BalancedWordsCounterTest {
+    public static void main(String args[]) {
+
+        System.out.println("1.input = \"aabbabcccba\"  result = "+ new BalancedWordsCounter().count("aabbabcccba"));
+        System.out.println("2.input=\"\"   result = "+ new BalancedWordsCounter().count(""));
+
+        try{
+            System.out.println("3.input = abababa1   result = "+ new BalancedWordsCounter().count("abababa1"));
+        }catch (Exception e){
+            System.out.println("3.input = abababa1   result = "+ e.getMessage());
+        }
+
+        try{
+            System.out.println("4.input = null   result = " + (new BalancedWordsCounter().count(null)));
+        }catch (Exception e){
+            System.out.println("4.input = null   result = "+e.getMessage());
+        }
+    }
+
 }
